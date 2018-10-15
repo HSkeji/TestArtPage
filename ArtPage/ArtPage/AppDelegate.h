e9bd81945d4608ef798afb993f031f919334adec
HSKJ <huashanedu@163.com>
2018-10-15 21:43:11 +0800

    初始化项目

---
+++ ArtPage/ArtPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ArtPage
+//
+//  Created by 王泽 on 2018/10/15.
+//  Copyright © 2018年 Huashankeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArtPage/ArtPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArtPage
+//
+//  Created by 王泽 on 2018/10/15.
+//  Copyright © 2018年 Huashankeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
